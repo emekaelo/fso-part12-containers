8c1f09a0a8d248d58bdad003c47440b4c3beeb54
emekaelo <elochux@gmail.com>
2024-05-15 03:45:04 +0100

    Exercise 12.15: Set up a frontend development environment completed
    To fix hot reload across host to container for vite :https://github.com/vitejs/vite/issues/1153#issuecomment-785467271

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true // Fixes hot reload across host to container for vite :https://github.com/vitejs/vite/issues/1153#issuecomment-785467271
+    }
+  }
 })
